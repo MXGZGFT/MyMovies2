15d43c4deb894099e44b17c2e93e107d345f6aa7
Max <mxgz@gft.com>
2022-09-07 09:55:45 +0000

    First commit

---
+++ src/main/java/com/mymovies/MyMovies2Application.java
@@ -0,0 +1,13 @@
+package com.mymovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMovies2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMovies2Application.class, args);
+	}
+
+}
